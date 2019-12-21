41747265b305c997ce919761164d3aedf47df835
EriccLiu <1486587187@qq.com>
2019-12-21 14:41:29 +0800

    after the fifth experiment

---
+++ ccpe-2014-experiments/collect_log.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+    echo "You must specify the name of the log collection file name"
+    exit 1
+fi
+
+
+mkdir "/root/$1"
+cd "/root/$1"
+
+mkdir "controller"
+cp -rb /var/log/neat/* controller
+for i in 1 2 3 4
+do
+mkdir "compute$i"
+scp compute$i:/var/log/neat/* compute$i
+done
+
+cd /root/ccpe-2014-experiments/results
+./db-dump
+cp -b db.sql /root/$1
+cp -b db.tar.gz /root/$1
